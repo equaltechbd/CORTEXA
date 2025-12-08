441713c0d7d9ac4861665406991bf5d5680fb61d
equaltechbd <equaltech.bd@gmail.com>
2025-12-08 08:22:26 +0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // এই লাইনটি যোগ করা হলো
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
